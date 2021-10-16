cdead7111478ee8b95e77a643259b449c2d8d201
JVROHIT0816 <77572267+JVROHIT0816@users.noreply.github.com>
2021-10-16 20:51:45 +0530

    fifth commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator/check');
 const router = express.Router();
-//@route  GET api/users
-//@desc   Test route
+//@route  POST api/users
+//@desc   Registration of Users
 //@access Public
-router.get('/', (req,res)=> res.send(`User Route`));
+router.post('/', 
+[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a Valid Email').isEmail(),
+    check('password', 'Should be 6 or more charectors').isLength({min:6})
+], 
+(req,res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
+    }
+    console.log(req.body);
+    res.send(`User Route`);
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 connectDB();
 
 
+// INIT Middleware
+app.use(express.json({extended:false}));
+
 app.get('/', (req,res)=>res.send(`first one`));
 
 //Define Routes
